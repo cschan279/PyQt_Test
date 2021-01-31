cb5a6024602d10dac0f800511554e521aeb75ad2
TroyChan <troychan279@gmail.com>
2021-01-31 21:10:16 +0800

    clear

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 translate = QtCore.QCoreApplication.translate
-from MainWindowF import Ui_MainWindow
+from slide_test import Ui_MainWindow
 import sys
-import resource
-
-
-class CamBox(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent=parent)
-        uic.loadUi('Frame.ui', self)
-        return
-
-
-stylesheet = """
-    QFrame#frame {
-        background-color: rgb(255, 255, 255); 
-        border: 1px inset black;
-    }
-"""
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -25,16 +9,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         # uic.loadUi('MainWindow.ui', self)
         self.setupUi(self)
-        self.boxes = list()
-        for i in range(10):
-            self.boxes.append(CamBox(self))
-            self.gridLayout.addWidget(self.boxes[i])
-        self.boxes[3].Indicator.setText('<img src=":/indicator/images/R-ori.png"/>')
+        self.horizontalSlider.hover.connect(lambda e: self.hover_event(e))
+
+    def hover_event(self, event):
+        print("hover", event)
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    # app.setStyleSheet(stylesheet)
     main_ = MainWindow()
     main_.show()
     app.exec_()
