1333f512e2c7002a7e79e22edf76898dd261292c
TroyChan <troychan279@gmail.com>
2021-02-01 22:07:01 +0800

    test_btn: fail(slow)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 
-if __name__ == "__main__":
+def testText():
     root = Tk()
     text_widget = Text(root)
     text_widget.pack(side=LEFT, fill=BOTH)
@@ -9,4 +9,61 @@ if __name__ == "__main__":
         text_widget.window_create(INSERT, window=Button(text_widget, text=f"{i:03d}"))
     text_widget.configure(state="disabled")
     # text_widget.configure(state="normal")
+    root.mainloop()
+    return
+
+######################################################################
+def grid_align():
+    root = Tk()
+    root.grid_columnconfigure(0, weight=0)
+    root.grid_columnconfigure(1, weight=3)
+    root.grid_columnconfigure(2, weight=1)
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_rowconfigure(1, weight=2)
+    btn_ls = list()
+    for i in range(6):
+        btn_ls.append(Button(root, text=f"{i:03d}"))
+        btn_ls[-1].grid(column=i%3, row=i//3, sticky="nsew")
+    root.mainloop()
+    return
+##########################################################
+
+
+
+
+
+def button_change_image(parent):
+    global btn, image_normal
+    btn = Button(root, image=image_normal)
+    btn.pack()
+    btn.bind("<Enter>", hover_btn)
+    btn.bind("<Leave>", leave_btn)
+    return
+
+
+def hover_btn(event):
+    print(event)
+    btn.configure(image=image_hover)
+    btn.image = image_hover
+    return
+
+
+def leave_btn(event):
+    print(event)
+    btn.configure(image=image_normal)
+    btn.image = image_normal
+    return
+
+################################################
+
+
+if __name__ == "__main__":
+    root = Tk()
+    # testText()
+    # grid_align()
+    image_normal = PhotoImage(file="ball_normal.png")
+    image_hover = PhotoImage(file="ball_hover.png")
+    image_press = PhotoImage(file="ball_press.png")
+    btn = None
+    button_change_image(root)
     root.mainloop()
